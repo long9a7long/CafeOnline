a6a6def7b50d7ebdf86ee461825b37ddf898ab8e
long9a7long <truonglongsometimes2297@gmail.com>
2018-12-12 12:13:57 +0700

    bill page by long

---
+++ CafeOnline/CafeOnline/Areas/Admin/Controllers/BillController.cs
@@ -0,0 +1,20 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeOnline.Areas.Admin.Controllers
+{
+    public class BillController : Controller
+    {
+        // GET: Admin/Bill
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var dao = new BillDao();
+            var model = dao.ListAllPaging(page, pageSize);
+            return View(model);
+        }
+    }
+}
